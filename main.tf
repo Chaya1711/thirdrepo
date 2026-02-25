17dfde55bca8255f7061430b24d9cc4e722a1623
Chay1711 <chayajayanu@gmail.com>
2026-02-26 00:49:48 +0530

    added s33

--- main.tf
+++ main.tf
@@ -1,12 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-}   
-resource "aws_instance" "excdample" {
-  ami = var.ami_id
-  instance_type = var.instance_type #type
-  subnet_id = var.subnet_cd
 }
 
-#resource "aws_s3_bucket" "s3_bucket" {
- # bucket = "my-terraform-chaya-state-bucket"
-#}
+module "ec2_instance" {
+  source = "./module/ec2_instance"
+}
