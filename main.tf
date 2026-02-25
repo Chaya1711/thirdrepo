20ffe1f23016002833339436eb0b097175d9616a
Chay1711 <chayajayanu@gmail.com>
2026-02-26 00:53:44 +0530

    added s33

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
+
 module "ec2_instance" {
-  source = "./module/ec2_instance"
+source = "./module/ec2_instance"
+ami_id = "ami-0f3caa1cf4417e51b"
+instance_type = "t3.micro"
+subnet_cd = "subnet-01ed0bf481ab409a2"
 }
